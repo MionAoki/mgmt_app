03bca5dd97540e9e5b3fe8e928bb266715be3997
MionAoki <mionaoki.0117@gmail.com>
2022-02-14 18:45:22 +0900

    add bottom transition

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,13 @@ class MyApp extends StatelessWidget{
   }
 }
 
-class Home extends StatelessWidget{
+class Home extends StatefulWidget{
+  @override
+  _HomeState createState() => _HomeState();
+}
 
+
+class _HomeState extends State<Home>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
