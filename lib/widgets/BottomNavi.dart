f32815bb2c85b50c35c0ffd821a794c36ca934e8
MionAoki <mionaoki.0117@gmail.com>
2022-02-15 11:08:29 +0900

    change bottomNavigationBar

--- lib/widgets/BottomNavi.dart
+++ lib/widgets/BottomNavi.dart
@@ -3,39 +3,24 @@ import 'package:flutter/material.dart';
 
 //bottom
 class BottomNavi extends BottomNavigationBar{
-    BottomNavi():super(
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.add_box),
-                label: 'Add contents',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.format_list_bulleted),
-                label: 'List',
-              ),
-            ],
-            selectedItemColor: Colors.amber[800],
-    );
+  BottomNavi():super(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_box),
+              label: 'Add contents',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.format_list_bulleted),
+              label: 'List',
+            ),
+          ],
+          selectedItemColor: Colors.amber[800],
+          //currentIndex: _currentIndex,
+          //onTap: _onTapped,
+  );
 }
 
-
-/*
-//bottomでのページ遷移
-class PageTrans extends StatefulWidget{
-  @override
-  _PageTransState createState() => _PageTransState();
-}
-
-class _PageTransState extends State<PageTrans>{
-  int _currentIndex = 0;
-  void _onTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-}
-*/
